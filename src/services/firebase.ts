8b8416682abde876d5bb3d182050e0527833aaa0
Diogo Marques <dimarques@gmail.com>
2021-09-01 16:01:29 -0300

    Components, firebase(9.0), pages, styles, ButtonHTMLAtributes (botão para ser reutilizado

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+// In order to keep your code functioning after updating your dependency from v8 to v9 beta, 
+//change your import statements to use the "compat" version of each import.
+
+import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();
